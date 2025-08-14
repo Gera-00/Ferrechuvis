1c3da7e9fe949dcb55c91d1f092676127c0faea3
Gerardo Ayon Gomez <aggo240173@upemor.edu.mx>
2025-08-14 01:53:31 -0600

    Primer commit, iniciando proyecto

---
+++ src/main/java/org/upemor/ferrechuvis/Ferrechuvis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.upemor.ferrechuvis;
+
+/**
+ *
+ * @author PC
+ */
+public class Ferrechuvis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
